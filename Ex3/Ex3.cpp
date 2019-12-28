5ed58551f77416876320c65d4ae69ee48783ad96
Hussein Merhi <hmerhi92@hotmail.com>
2019-12-28 16:31:03 +0200

    Ex4 Complete

--- Ex3/Ex3.cpp
+++ Ex3/Ex3.cpp
@@ -21,6 +21,14 @@ typedef struct dlist
     dlist* precedent;
 } dlist;
 
+typedef struct PI
+{
+    double operand;
+    char operation;
+    bool estOperand;
+    PI* suivant;
+} PI;
+
 pile* Empiler(pile* p, double valeur)
 {
     pile* p1 = new pile;
@@ -51,6 +59,73 @@ bool EstVide(pile* p)
     }
 }
 
+void AfficherPile(pile* p)
+{
+    if (p != NULL)
+    {
+        cout << " " << p->valeur;
+        AfficherPile(p->suivant);
+    }
+    else
+    {
+        cout << endl;
+    }
+}
+
+PI* EmpilerOperand(PI* p, double valeur)
+{
+    PI* p1 = new PI;
+    p1->operand = valeur;
+    p1->estOperand = true;
+    p1->suivant = p;
+    return p1;
+}
+
+PI* EmpilerOperation(PI* p, char op)
+{
+    PI* p1 = new PI;
+    p1->operation = op;
+    p1->estOperand = false;
+    p1->suivant = p;
+    return p1;
+}
+
+PI* DepilerPI(PI* p)
+{
+    return p->suivant;
+}
+
+double PeekOperand(PI* p)
+{
+    return p->operand;
+}
+
+char PeekOperation(PI* p)
+{
+    return p->operation;
+}
+
+void AfficherPI(PI* p)
+{
+    if (p != NULL)
+    {
+        if (p->estOperand)
+        {
+            cout << " " << p->operand;
+        }
+        else
+        {
+            cout << " ";
+            cout << p->operation;
+        }
+        AfficherPI(p->suivant);
+    }
+    else
+    {
+        cout << endl;
+    }
+}
+
 void AfficherDList(dlist* dl)
 {
     if (dl != NULL)
@@ -61,6 +136,18 @@ void AfficherDList(dlist* dl)
     else
     {
         cout << endl;
+    }
+}
+
+int TailleDList(dlist* dl)
+{
+    if (dl != NULL)
+    {
+        return 1 + TailleDList(dl->suivant);
+    }
+    else
+    {
+        return 0;
     }
 }
 
@@ -186,19 +273,248 @@ dlist* ListeParAlternance(dlist* Dlist1, dlist* Dlist2)
     return dl;
 }
 
-dlist* TrierCroissant(dlist* Dlist)
+void TrierCroissant(dlist* dl)
 {
+    dlist* curr = dl;
+    int taille = TailleDList(dl);
+    for (int i = 0; i < taille - 1; i++)
+    {
+        dlist* tmp = dl;
+        for (int j = 0; j < i; j++)
+        {
+            tmp = tmp->suivant;
+        }
+        while (tmp != NULL)
+        {
+            if (curr->valeur > tmp->valeur)
+            {
+                double vtmp = curr->valeur;
+                curr->valeur = tmp->valeur;
+                tmp->valeur = vtmp;
+            }
+            tmp = tmp->suivant;
+        }
+        curr = curr->suivant;
+    }
+}
 
+// 2- 
+void getReplaceHalf(dlist* dl)
+{
+    dlist* curr = dl;
+    double temp;
+    while (curr != NULL)
+    {
+        if (modf(curr->valeur, &temp) == 0 && (int)curr->valeur % 2 == 0)
+        {
+            dlist* nouveau = new dlist;
+            double moitie = curr->valeur / 2;
+            curr->valeur = moitie;
+            nouveau->valeur = moitie;
+            nouveau->suivant = curr->suivant;
+            curr->suivant = nouveau;
+            curr = curr->suivant;
+        }
+        curr = curr->suivant;
+    }
+}
+
+// 3-
+void SeparerPairEtImpair(dlist* dl, pile** p1, pile** p2)
+{
+    dlist* curr = dl;
+    double temp;
+    while (curr != NULL)
+    {
+        if (modf(curr->valeur, &temp) == 0 && (int)curr->valeur % 2 == 0)
+        {
+            *p1 = Empiler(*p1, curr->valeur);
+        }
+        else
+        {
+            *p2 = Empiler(*p2, curr->valeur);
+        }
+        curr = curr->suivant;
+    }
+}
+
+// 4-
+double Calculer(double a, double b, char op)
+{
+    switch (op)
+    {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        if (b == 0)
+        {
+            return NULL;
+        }
+        else
+        {
+            return a / b;
+        }
+    default:
+        return NULL;
+    }
+}
+
+double CalculerPI(PI* exp)
+{
+    PI* curr = exp;
+    PI* memoire = NULL;
+    while (curr != NULL)
+    {
+        if (curr->estOperand)
+        {
+            double operand = PeekOperand(curr);
+            curr = DepilerPI(curr);
+            memoire = EmpilerOperand(memoire, operand);
+        }
+        else
+        {
+            char op = PeekOperation(curr);
+            curr = DepilerPI(curr);
+            double a = PeekOperand(memoire);
+            memoire = DepilerPI(memoire);
+            double b = PeekOperand(memoire);
+            memoire = DepilerPI(memoire);
+            double resulat = Calculer(a, b, op);
+            memoire = EmpilerOperand(memoire, resulat);
+        }
+    }
+    return memoire->operand;
+}
+
+int operandsPI(PI* p)
+{
+    int operandNB = 0;
+    PI* curr = p;
+    while (curr != NULL)
+    {
+        if (curr->estOperand)
+        {
+            operandNB++;
+        }
+        curr = curr->suivant;
+    }
+    return operandNB;
+}
+
+int operationsPI(PI* p)
+{
+    int operationsNB = 0;
+    PI* curr = p;
+    while (curr != NULL)
+    {
+        if (!curr->estOperand)
+        {
+            operationsNB++;
+        }
+        curr = curr->suivant;
+    }
+    return operationsNB;
+}
+
+bool ValiderPI(PI* p)
+{
+    if (operandsPI(p) != (operationsPI(p) + 1))
+    {
+        return false;
+    }
+    else
+    {
+        int indice = 0;
+        PI* curr = p;
+        while (curr != NULL)
+        {
+            if (curr->estOperand)
+            {
+                indice++;
+            }
+            else
+            {
+                indice--;
+            }
+            if (indice < 0)
+            {
+                return false;
+            }
+            curr = curr->suivant;
+        }
+        if (indice != 1)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
+void CompterOperandEtOperation(PI* p)
+{
+    int operandNB = 0;
+    int operationNB = 0;
+    PI* curr = p;
+    while (curr != NULL)
+    {
+        if (curr->estOperand)
+        {
+            operandNB++;
+        }
+        else
+        {
+            operationNB++;
+        }
+        curr = curr->suivant;
+    }
+    cout << operandNB << " operands" << endl;
+    cout << operationNB << " operations" << endl;
 }
 
 int main()
 {
+    pile* p1 = NULL;
+    pile *p2 = NULL;
     dlist* racines = ListeDeRacineDeNewton(7);
     dlist* rationals = ListeDeRationals(5, 2);
     dlist* alternance = ListeParAlternance(racines, rationals);
     AfficherDList(racines);
     AfficherDList(rationals);
     AfficherDList(alternance);
-    
+    TrierCroissant(alternance);
+    AfficherDList(alternance);
+    getReplaceHalf(alternance);
+    AfficherDList(alternance);
+    SeparerPairEtImpair(alternance, &p1, &p2);
+    AfficherPile(p1);
+    AfficherPile(p2);
+
+    // Notation Polonaise Inversee
+    PI* expression = NULL;
+    expression = EmpilerOperation(expression, '*');
+    expression = EmpilerOperation(expression, '+');
+    expression = EmpilerOperation(expression, '*');
+    expression = EmpilerOperation(expression, '+');
+    expression = EmpilerOperand(expression, 4);
+    expression = EmpilerOperand(expression, 6);
+    expression = EmpilerOperation(expression, '+');
+    expression = EmpilerOperand(expression, 8);
+    expression = EmpilerOperand(expression, 9);
+    expression = EmpilerOperand(expression, 7);
+    expression = EmpilerOperand(expression, 5);
+    AfficherPI(expression);
+    cout << CalculerPI(expression) << endl;
+    if (ValiderPI(expression))
+    {
+        cout << "Expression est valide" << endl;
+    }
+    else
+    {
+        cout << "Expression n'est pas valide" << endl;
+    }
+    CompterOperandEtOperation(expression);
 }
-asdawdasdawsdawdsada
---
+++ Ex4/Ex4.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct arbre
+{
+    char cle;
+    arbre* gauche = NULL;
+    arbre* droite = NULL;
+} arbre;
+
+bool AAucuneFeuilles(arbre* a)
+{
+    if (a->gauche == NULL && a->droite == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool ADeuxFeuilles(arbre* a)
+{
+    if (a->gauche != NULL && a->droite != NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool EstNumero(char c)
+{
+    switch (c)
+    {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool EstOperation(char c)
+{
+    switch (c)
+    {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        return true;
+    default:
+        return false;
+    }
+}
+
+// 1-
+void AfficherInfix(arbre* a, bool premiere)
+{
+    if (a != NULL)
+    {
+        bool avoirParenthese = !premiere && ADeuxFeuilles(a);
+        if (avoirParenthese)
+        {
+            cout << "(";
+        }
+        AfficherInfix(a->gauche, false);
+        cout << a->cle;
+        AfficherInfix(a->droite, false);
+        if (avoirParenthese)
+        {
+            cout << ")";
+        }
+    }
+}
+
+// 2-
+double Eval(arbre* a)
+{
+    if (AAucuneFeuilles(a))
+    {
+        return (double)a->cle - 48;
+    }
+    else
+    {
+        switch (a->cle)
+        {
+        case '+':
+            return Eval(a->gauche) + Eval(a->droite);
+        case '-':
+            return Eval(a->gauche) - Eval(a->droite);
+        case '*':
+            return Eval(a->gauche) * Eval(a->droite);
+        case '/':
+            return Eval(a->gauche) / Eval(a->droite);
+        }
+    }
+}
+
+// 3-
+bool ValiderArbre(arbre* a)
+{
+    if (a != NULL)
+    {
+        if (EstOperation(a->cle) && ADeuxFeuilles(a))
+        {
+            return true && ValiderArbre(a->gauche) && ValiderArbre(a->droite);
+        }
+        else if (EstNumero(a->cle) && AAucuneFeuilles(a))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+// 4-
+void ParcoursProfondeur(arbre* a)
+{
+    if (a != NULL)
+    {
+        // Mark as visitied
+
+        // Print it
+        cout << a->cle;
+        // Goes for adjacents
+        ParcoursProfondeur(a->gauche);
+        ParcoursProfondeur(a->droite);
+    }
+}
+
+// 5-
+arbre* RotationGauche(arbre* a)
+{
+    if (a->droite != NULL)
+    {
+        arbre* tmp = a->droite;
+        a->droite = NULL;
+        a->droite = tmp->gauche;
+        tmp->gauche = a;
+        return tmp;
+    }
+    else
+    {
+        return a;
+    }
+}
+
+int main()
+{
+    // construire ((A - B) * (C + D / E)) - F
+    // ((9 - 5) * (2 + 6 / 3)) - 7 = 13
+    
+    arbre* a = new arbre;
+    arbre* curr = a;
+    curr->cle = '-';
+
+    curr->droite = new arbre;
+    curr->gauche = new arbre;
+    curr->droite->cle = '7';
+    curr->gauche->cle = '*';
+
+    curr = curr->gauche;
+    curr->gauche = new arbre;
+    curr->droite = new arbre;
+    curr->gauche->cle = '-';
+    curr->droite->cle = '+';
+
+    curr->gauche->gauche = new arbre;
+    curr->gauche->droite = new arbre;
+    curr->gauche->gauche->cle = '9';
+    curr->gauche->droite->cle = '4';
+    
+    curr = curr->droite;
+    curr->gauche = new arbre;
+    curr->droite = new arbre;
+    curr->gauche->cle = '2';
+    curr->droite->cle = '/';
+
+    curr = curr->droite;
+    curr->gauche = new arbre;
+    curr->droite = new arbre;
+    curr->gauche->cle = '6';
+    curr->droite->cle = '3';
+
+    if (ValiderArbre(a))
+    {
+        cout << "cet arbre est valide" << endl;
+    }
+    else
+    {
+        cout << "cet arbre n'est pas valide" << endl;
+    }
+
+    AfficherInfix(a, true); cout << " = " << Eval(a) << endl;
+    ParcoursProfondeur(a); cout << endl;
+
+    cout << "Arbre Initial : "; AfficherInfix(a, true); cout << endl;
+    a->gauche = RotationGauche(a->gauche);
+    cout << "Apres Rotation : "; AfficherInfix(a, true); cout << endl;
+}
+
